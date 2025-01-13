f8f89f53298bd16d66dba29e8892c9284783c763
ChanelKiska <artemmatiik3@gmail.com>
2025-01-14 02:38:25 +0300

    smooth.js

--- smooth.js
+++ smooth.js
@@ -4,3 +4,16 @@ document.getElementById("scrollToPost").onclick = function(event) {
         behavior: "smooth" // Добавляем плавный эффект прокрутки
     });
 };
+ <script>// Инициализация Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAox_pDBk89L8wrFm4lT5g252yCEE7AP2g",
+    authDomain: "creatematiik.firebaseapp.com",
+    databaseURL: "https://~2FLike~2FsPd40DP3S4jtl8YCFb4c.firebaseio.com/",
+    projectId: "creatematiik",
+    storageBucket: "creatematiik.firebasestorage.app",
+    messagingSenderId: "788527130354",
+    appId: "1:788527130354:web:e05785cffb92cef542526b"
+};
+
+// Инициализируем Firebase
+firebase.initializeApp(firebaseConfig)</script>
